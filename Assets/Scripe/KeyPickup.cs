6cdc7f6fabca37e424a5f6958d6a1fee67d4938c
rudebunnii <kimkup53@gmail.com>
2026-04-27 21:05:01 +0700

    add-sfx-to-scripts

--- Assets/Scripe/KeyPickup.cs
+++ Assets/Scripe/KeyPickup.cs
@@ -1,11 +1,19 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class KeyPickup : MonoBehaviour
 {
+    public AudioClip pickupSound; // 🎵 ใส่ไฟล์เสียงเก็บของ
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            // เล่นเสียงตรงตำแหน่งกุญแจก่อนจะ Destroy ตัวเอง
+            if (pickupSound != null)
+            {
+                AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+            }
+
             other.GetComponent<PlayerInventory>().hasKey = true;
             Destroy(gameObject);
         }
