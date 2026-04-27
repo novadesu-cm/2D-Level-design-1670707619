3f79ff8585bd36f69dd30deed506ce52ccc9522f
rudebunnii <kimkup53@gmail.com>
2026-04-27 20:45:21 +0700

    stage-1-fix

---
+++ Assets/Scripe/Portal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // 👈 จำเป็นต้องมีเพื่อใช้คำสั่งเปลี่ยนด่าน
+
+public class Portal : MonoBehaviour
+{
+    [Header("ชื่อฉากที่จะวาร์ปไป")]
+    public string sceneName = "PLAY 2";
+
+    [Header("เอฟเฟกต์ (ถ้ามี)")]
+    public ParticleSystem teleportEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // เช็คว่าผู้เล่นเดินมาชนประตูหรือไม่
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("วาร์ปไปด่าน: " + sceneName);
+
+            if (teleportEffect != null)
+            {
+                teleportEffect.Play();
+            }
+
+            // สั่งโหลดฉากใหม่ทันที
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
